28aeceaea5a5a3335f388a61e9f4ac2b9dcf80c0
winnyace <winnyace@proton.me>
2025-03-08 16:16:17 +0200

    some cleanup and new fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,27 @@
 # winny's config.fish
-
+#
 # Starship startup
 starship init fish | source
+
 
 # Removing the greeting message from fish
 function fish_greeting
     clear
 end
 
+# Adding !! to fish
+function last_history_item
+    echo $history[1]
+end
+abbr -a !! --position anywhere --function last_history_item 
+
+
 # Aliases
 alias imgcat="kitty +kitten icat --align left" # only with kitty
 alias fm="nnn -P x"
 alias ls="eza"
 alias sysinfo="fastfetch --config neofetch.jsonc"
+
 
 # Environment variables
 # Theme related
